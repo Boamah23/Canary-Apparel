b6d3c795bf8f0a1bd8af933c6ac553a36964c0cd
boamahn <boamahn@uni.coventry.ac.uk>
2019-09-18 15:58:24 +0100

    V1

---
+++ CanaryApparel/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CanaryApparel
+//
+//  Created by nathan boamah on 18/09/2019.
+//  Copyright © 2019 nathan boamah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
