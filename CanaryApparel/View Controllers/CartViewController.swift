a539c84673066822622e5b4b41aaa4d09914050e
boamahn <boamahn@uni.coventry.ac.uk>
2019-09-22 00:47:06 +0100

    Firebase pod integration

---
+++ CanaryApparel/View Controllers/CartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CartViewController.swift
+//  CanaryApparel
+//
+//  Created by nathan boamah on 21/09/2019.
+//  Copyright © 2019 nathan boamah. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-CanaryApparelUITests/Pods-CanaryApparelUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CanaryApparelUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CanaryApparelUITests
+@end
