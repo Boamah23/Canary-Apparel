6a66e5bfff524eeed16879a2419f144fb10a6b9b
boamahn <boamahn@uni.coventry.ac.uk>
2019-09-21 22:01:08 +0100

    Updated sign in and login constraints

---
+++ CanaryApparel/FavouritesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavouritesTableViewCell.swift
+//  CanaryApparel
+//
+//  Created by nathan boamah on 20/09/2019.
+//  Copyright © 2019 nathan boamah. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
