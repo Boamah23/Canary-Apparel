bfa22754be728ecd12034e1836bff5be3b8f6b50
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-06 04:02:53 +0000

    Create ProductTableViewCell.swift

---
+++ CanaryApparel/View Controllers/ProductTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ProductTableViewCell.swift
+//  CanaryApparel
+//
+//  Created by nathan boamah on 06/12/2019.
+//  Copyright © 2019 nathan boamah. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productLabel: UILabel!
+    
+    
+    var product: Product? {
+        didSet {
+            self.update()
+        }
+    }
+    
+    func update() {
+        productImageView.image = product?.image
+        productLabel.text = product?.title
+    }
+}
