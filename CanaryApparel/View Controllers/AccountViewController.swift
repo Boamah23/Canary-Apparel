c2509adb2364a80a8d64e1f809bf7880336b35d7
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-06 14:27:12 +0000

    Update AccountViewController.swift

--- CanaryApparel/View Controllers/AccountViewController.swift
+++ CanaryApparel/View Controllers/AccountViewController.swift
@@ -7,27 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class AccountViewController: UIViewController {
 
     
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //button action
+    @IBAction func signOutTapped(_ sender: Any){
+        do{
+            //signs out user
+            try Auth.auth().signOut()
+        }catch{
+            print(error)
+        }
     }
-    */
+    
+    
+    
 
 }
