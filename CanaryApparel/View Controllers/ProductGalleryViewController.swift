00450a1f2ac72f378d4eb4c77d06a49fc2e3fd0b
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-06 14:27:39 +0000

    Update ProductGalleryViewController.swift

--- CanaryApparel/View Controllers/ProductGalleryViewController.swift
+++ CanaryApparel/View Controllers/ProductGalleryViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 
 class ProductGalleryViewController: UIViewController {
-    
+    //stores an array of images
     var collectionData = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8"]
     
     let productImages: [UIImage] = [
@@ -22,9 +22,9 @@ class ProductGalleryViewController: UIViewController {
         UIImage(named: "image7")!,
         UIImage(named: "image8")!,
     ]
-    
+    //stores array of prices
     let prices = [("£26.00"), ("£16.00"), ("£28.00"), ("£34.00"), ("£38.00"), ("£36.00"), ("£20.00"), ("£30.00")]
-    
+    //stores array of descriptions
     let descriptions = [("Cropped Levi's t-shirt"),("Cropped vest top"),("White and black stripped top"),("Mustard long-line shirt"),("Denim dungarees"),("White bodysuit"),("Hipster white t-shirt"),("white denim dungarees")]
     
     
@@ -38,12 +38,13 @@ class ProductGalleryViewController: UIViewController {
 
 }
 
-
+//determines the amount of items in section
 extension ProductGalleryViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //counts amount of items in array
         return collectionData.count
     }
-    
+    //Returns a reusable table-view cell object located by its identifier.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
         cell.productImage.image = productImages[indexPath.item]
@@ -51,7 +52,7 @@ extension ProductGalleryViewController: UICollectionViewDelegate, UICollectionVi
         cell.productDescription.text = descriptions[indexPath.item]
     return cell
     }
-    
+    //passes data to detail view controller
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let vc = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController
         vc?.img = productImages[indexPath.item]
