95bdc7e57db739c377709e0cc7b405d826d1c9cd
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-06 16:28:07 +0000

    Update CanaryApparelUITests.swift

--- CanaryApparelUITests/CanaryApparelUITests.swift
+++ CanaryApparelUITests/CanaryApparelUITests.swift
@@ -2,7 +2,7 @@
 //  CanaryApparelUITests.swift
 //  CanaryApparelUITests
 //
-//  Created by nathan boamah on 19/09/2019.
+//  Created by nathan boamah on 06/12/2019.
 //  Copyright © 2019 nathan boamah. All rights reserved.
 //
 
@@ -16,9 +16,6 @@ class CanaryApparelUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
@@ -27,15 +24,63 @@ class CanaryApparelUITests: XCTestCase {
     }
 
     func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    
+    func testExample1() {
         
         let app = XCUIApplication()
-        app.tabBars.buttons["Search"].tap()
-        app.scrollViews.otherElements.containing(.image, identifier:"holiday").children(matching: .button).element(boundBy: 0).tap()
-        app.collectionViews.cells.otherElements.containing(.staticText, identifier:"£26.00").children(matching: .image).element.tap()
-        app.textFields["Select size"].tap()
-        app/*@START_MENU_TOKEN@*/.pickerWheels["Extra Small - XS"]/*[[".pickers.pickerWheels[\"Extra Small - XS\"]",".pickerWheels[\"Extra Small - XS\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.toolbars["Toolbar"].buttons["Done"].tap()
+        app.buttons["SIGN IN"].tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.textFields["Email"].tap()
+        
+        let passwordSecureTextField = elementsQuery.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.tap()
+        elementsQuery/*@START_MENU_TOKEN@*/.staticTexts["LOGIN"]/*[[".buttons[\"LOGIN\"].staticTexts[\"LOGIN\"]",".staticTexts[\"LOGIN\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+         
         
     }
+    
+   
+    func testExample2() {
+       
+        
+        let app = XCUIApplication()
+        app.buttons["SIGN IN"].tap()
+        
+        let scrollViewsQuery = app.scrollViews
+        let elementsQuery = scrollViewsQuery.otherElements
+        elementsQuery.textFields["Email"].tap()
+        
+        let passwordSecureTextField = elementsQuery.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.tap()
+        elementsQuery/*@START_MENU_TOKEN@*/.staticTexts["LOGIN"]/*[[".buttons[\"LOGIN\"].staticTexts[\"LOGIN\"]",".staticTexts[\"LOGIN\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        scrollViewsQuery.otherElements.containing(.image, identifier:"holiday").children(matching: .button).element(boundBy: 0).tap()
+        app.navigationBars["CanaryApparel.ProductGalleryView"].buttons["Back"].tap()
+        app.navigationBars["CanaryApparel.SearchView"].buttons["Back"].tap()
+        app.navigationBars["CanaryApparel.SignInView"].buttons["Back"].tap()
+        app.tabBars.buttons["Account"].tap()
+        app.buttons["SIGN OUT"].tap()
+        
+    }
+    
 
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 }
